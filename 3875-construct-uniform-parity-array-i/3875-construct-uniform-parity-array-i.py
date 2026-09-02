1b7414854b0b98999827a0491e7d9a228d55d853
VishalNyk <123799361+VishalNyk@users.noreply.github.com>
2026-09-03 01:11:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (47.62%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        #if nums1 contains any odd number, operation 2 will always 
+        #get odd result based on the fact that we check every element
+        # if odd element ,execute operation 1 and if even , do operation
+        # as any odd number where j!=i and even-odd=odd only
+
+        #hence always true
+
+        return True
+        
