ed296e3f5e881601c8041fcfada2515aeafe25ab
VishalNyk <123799361+VishalNyk@users.noreply.github.com>
2026-09-07 17:17:19 +0530

    Time: 0 ms (100%), Space: 19.3 MB (53.49%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.py
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -1,21 +1,16 @@
 class Solution:
-    def numberOfArithmeticSlices(self, nums: list[int]) -> int:
-        if len(nums) < 3:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        if(len(nums)<3):
             return 0
-            
-        total_slices = 0
-        current_run = 0
-        
-        # We start at index 2 because a slice needs at least 3 elements
-        for i in range(2, len(nums)):
-            # Check if the last three elements form an arithmetic progression
-            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
-                # The sequence extends! Increment our running count of new slices
-                current_run += 1
-                # Add these newly formed slices to our grand total
-                total_slices += current_run
+        csum=0
+        cslice=0
+
+        for i in range(2,len(nums)):
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]):
+                csum+=1
+                cslice+=csum
             else:
-                # The arithmetic sequence broke. Reset the run counter.
-                current_run = 0
-                
-        return total_slices
+                csum=0
+        return cslice
+        
+        
